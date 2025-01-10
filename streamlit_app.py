b865c60e746afdb84a527e3cbde7f6d6d3c4b47e
LevaTNT <leva90nt@mail.ru>
2025-01-10 16:33:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+f = open("test.txt","rw")
 st.title("🎈 My new app")
 st.write(
     "HELLO FOR ALL 962 PROGRAMMERS! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
@@ -16,4 +16,6 @@ user_input = st.text_input("Введите первое число:")
 user_input2 = st.text_input("Введите второе число:")
 # Кнопка для подтверждения ввода
 if st.button("Сложить"):
+    a = int(user_input) +int(user_input2)
     st.write(f"Сумма чисел {user_input} и {user_input2} равна {int(user_input) +int(user_input2)}")
+f.write(f"{user_input} {user_input2} {a}")
