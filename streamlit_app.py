a87bb6cffcde52318dae56c049a763ec0b3db675
LevaTNT <leva90nt@mail.ru>
2025-01-10 16:49:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,26 @@
 import streamlit as st
-st.title("🎈 My new app")
-st.write(
-    "HELLO FOR ALL 962 PROGRAMMERS! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write(
-    "HELLO FOR ALL 962 PROGRAMMERS! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.title("🎈 My new app")
-# Заголовок приложения
-st.title("Пример ввода данных")
-
-# Текстовое поле для ввода данных
-user_input = st.text_input("Введите первое число:")
-user_input2 = st.text_input("Введите второе число:")
-# Кнопка для подтверждения ввода
-if st.button("Сложить"):
-    a = int(user_input) +int(user_input2)
-    st.write(f"Сумма чисел {user_input} и {user_input2} равна {int(user_input) +int(user_input2)}")
 import sqlite3
-
 # Подключение к базе данных (или создание новой)
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
-
 # Создание таблицы
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, city TEXT)''')
-
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)''')
 # Вставка данных
-cursor.execute('INSERT INTO users (name, age, city) VALUES (?, ?, ?)', ('Алиса', 25, 'Москва'))
+cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', ('Алиса', 25))
 conn.commit()
+st.title("Введите данные для ввода")
+# Текстовое поле для ввода данных
+user_input = st.text_input("Введите имя:")
+user_input2 = st.text_input("Введите возраст:")
+if st.button("Ввести"):
+    cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', (user_input, int(user_input2)))
+    conn.commit()
 
-# Чтение данных
-cursor.execute('SELECT * FROM users')
-rows = cursor.fetchall()
-for row in rows:
-    st.write(row)
-
+if st.button("Вывести"):
+    cursor.execute('SELECT * FROM users')
+    rows = cursor.fetchall()
+    for row in rows:
+        st.write(row)
+        
 # Закрытие соединения
 conn.close()
