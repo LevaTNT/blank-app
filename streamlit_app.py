1b113abce97fd8fb78f0a029cf6a6afea79b67d3
LevaTNT <leva90nt@mail.ru>
2025-01-10 16:26:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,12 @@ st.write(
     "HELLO FOR ALL 962 PROGRAMMERS! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.title("🎈 My new app")
+# Заголовок приложения
+st.title("Пример ввода данных")
+
+# Текстовое поле для ввода данных
+user_input = st.text_input("Введите что-нибудь:")
+
+# Кнопка для подтверждения ввода
+if st.button("Подтвердить"):
+    st.write(f"Вы ввели: {user_input}")
